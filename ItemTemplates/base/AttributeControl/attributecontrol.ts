0d814a37d6cecf8a2c90349d42d165365acf0dee
Donald Jones <donald@getplatypi.com>
2014-10-27 10:53:56 -0500

    Fixed plat reference in attribute control template.

--- ItemTemplates/base/AttributeControl/attributecontrol.ts
+++ ItemTemplates/base/AttributeControl/attributecontrol.ts
@@ -1,23 +1,25 @@
-module %name% {
-    class %name%AttributeControl extends plat.controls.AttributeControl {
-        /**
-         * The initialize event method for a control. This is event is called prior to compilation.
-         */
-        initialize() { }
-        
-        /**
-         * The loaded event method for a control. This event is fired after a control has been completely loaded,
-         * meaning all of its children have also been loaded and all DOM has been created and populated. It is now 
-         * safe for all controls to access, observe, and modify the context property.
-         */
-        loaded() { }
-        
-        /**
-         * The dispose event is called when a control is being removed from memory. A control should release 
-         * all of the memory it is using, including DOM event and property listeners.
-         */
-        dispose() { }
-    }
+/// <reference path="../../_references.d.ts" />
 
-    plat.register.control('%registername%', %name%AttributeControl);
+import plat = require('platypus');
+
+class %name%AttributeControl extends plat.controls.AttributeControl {
+    /**
+        * The initialize event method for a control. This is event is called prior to compilation.
+        */
+    initialize() { }
+        
+    /**
+        * The loaded event method for a control. This event is fired after a control has been completely loaded,
+        * meaning all of its children have also been loaded and all DOM has been created and populated. It is now 
+        * safe for all controls to access, observe, and modify the context property.
+        */
+    loaded() { }
+        
+    /**
+        * The dispose event is called when a control is being removed from memory. A control should release 
+        * all of the memory it is using, including DOM event and property listeners.
+        */
+    dispose() { }
 }
+
+plat.register.control('%registername%', %name%AttributeControl);
