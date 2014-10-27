a74287d7299efafb82b1ae34c5b9c636d8113e5a
Donald Jones <donald@getplatypi.com>
2014-10-27 10:43:42 -0500

    Added attribute control template.

---
+++ ItemTemplates/base/AttributeControl/attributecontrol.ts
@@ -0,0 +1,23 @@
+module %name% {
+    class %name%AttributeControl extends plat.controls.AttributeControl {
+        /**
+         * The initialize event method for a control. This is event is called prior to compilation.
+         */
+        initialize() { }
+        
+        /**
+         * The loaded event method for a control. This event is fired after a control has been completely loaded,
+         * meaning all of its children have also been loaded and all DOM has been created and populated. It is now 
+         * safe for all controls to access, observe, and modify the context property.
+         */
+        loaded() { }
+        
+        /**
+         * The dispose event is called when a control is being removed from memory. A control should release 
+         * all of the memory it is using, including DOM event and property listeners.
+         */
+        dispose() { }
+    }
+
+    plat.register.control('%registername%', %name%AttributeControl);
+}
