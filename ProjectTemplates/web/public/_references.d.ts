aa0695f891c8245a44f62658f190a4bd422472d6
Donald Jones <donald@getplatypi.com>
2014-12-04 16:10:51 -0600

    CLI-10 #time 5m Updated template reference file.

--- ProjectTemplates/web/public/_references.d.ts
+++ ProjectTemplates/web/public/_references.d.ts
@@ -2,8 +2,8 @@
 /// <reference path="../node_modules/platypus/platypus-node.d.ts" />
 
 // Models
-/// <reference path="models/server/iserver.model.ts" />
-/// <reference path="models/base/ibase.model.ts" />
+/// <reference path="models/server/iserver.model.d.ts" />
+/// <reference path="models/base/ibase.model.d.ts" />
 
 // Services
-/// <reference path="services/base/ibase.service.ts" />
+/// <reference path="services/base/ibase.service.d.ts" />
