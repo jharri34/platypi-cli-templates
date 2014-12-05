8432d68429c4ce2c2da7864910565bec490dc2c9
Donald Jones <donald@getplatypi.com>
2014-12-05 16:34:19 -0600

    CLI-10 #time 10m Updated references file for placeholders.

--- ProjectTemplates/web/public/_references.d.ts
+++ ProjectTemplates/web/public/_references.d.ts
@@ -1,9 +1,9 @@
 /// <reference path="typings/tsd.d.ts" />
 /// <reference path="../node_modules/platypus/platypus-node.d.ts" />
 
-// Models
+// models
 /// <reference path="models/server/iserver.model.d.ts" />
 /// <reference path="models/base/ibase.model.d.ts" />
 
-// Services
+// services
 /// <reference path="services/base/ibase.service.d.ts" />
