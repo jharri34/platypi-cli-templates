77b5b8361f4cf1d45387e58461dfef717b387ab4
pwdonald <donald@getplatypi.com>
2015-02-18 12:19:13 -0600

    Fixed a few misspellings.

--- ProjectTemplates/mobile/public/_references.d.ts
+++ ProjectTemplates/mobile/public/_references.d.ts
@@ -2,7 +2,7 @@
 
 // models
 /// <reference path="factories/server/iserver.factory.d.ts" />
-/// <reference path="factroies/base/ibase.factory.d.ts" />
+/// <reference path="factories/base/ibase.factory.d.ts" />
 
 // services
 /// <reference path="services/base/ibase.service.d.ts" />
