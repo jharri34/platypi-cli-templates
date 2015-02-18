7bb84da56bffffd5a5ec50e4df3f45a1098d9e70
Donald Jones <jones.donald@gmail.com>
2015-02-17 21:13:19 -0600

    LAUNCH-40 Added stringify to viewcontrol in project template.

--- ProjectTemplates/mobile/public/viewcontrols/home/home.viewcontrol.ts
+++ ProjectTemplates/mobile/public/viewcontrols/home/home.viewcontrol.ts
@@ -4,7 +4,7 @@ import plat = require('platypus');
 import BaseViewControl = require('../base/base.viewcontrol');
 
 class HomeViewControl extends BaseViewControl {
-    templateUrl = this.getTemplateUrl(__filename);
+    templateUrl = require('./%lowername%.viewcontrol.html');
 
     context = {};
 }
