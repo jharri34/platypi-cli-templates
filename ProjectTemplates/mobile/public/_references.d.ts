52713146f5a96d4fd505858fd126e524e325e7a2
pwdonald <donald@getplatypi.com>
2015-02-19 12:19:47 -0600

    Removed old reference to services.d.ts

--- ProjectTemplates/mobile/public/_references.d.ts
+++ ProjectTemplates/mobile/public/_references.d.ts
@@ -5,5 +5,3 @@
 /// <reference path="factories/server/iserver.factory.d.ts" />
 /// <reference path="factories/base/ibase.factory.d.ts" />
 
-// services
-/// <reference path="services/base/ibase.service.d.ts" />
